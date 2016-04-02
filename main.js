8f405371a87e81f993cc4b746093ba5eb0853dde
Alexandros Monastiriotis <alex.monastiriotis@gmail.com>
2016-04-03 00:14:04 +0300

    beautify js

--- main.js
+++ main.js
@@ -1,249 +1,247 @@
 'use strict';
 
-
 /**
  * Basic Models 
  *
  */
-var Task = function (argId, argName, argStatus) {
-	
-	return {
-		id : argId,
+var Task = function(argId, argName, argStatus) {
 
-		name : argName,
+    return {
+        id: argId,
 
-		status: argStatus,
+        name: argName,
 
-		execute : function () {
-			$('#log-txt').val(
-				$('#log-txt').val() + '\r\nExecuting task with id = ' + this.id
-			);
+        status: argStatus,
 
-			var post = $.ajax({
-			    url: "/index.php",
-			    data: {
-		    		execute : true,
-		    		id : this.id,
-		    		name : this.name,
-		    		status : this.status
-			    	
-		    	},			    
-			    type: "POST"
-			});
+        execute: function() {
+            $('#log-txt').val(
+                $('#log-txt').val() + '\r\nExecuting task with id = ' + this.id
+            );
 
-			return post;
-		}
-	};
+            var post = $.ajax({
+                url: "/index.php",
+                data: {
+                    execute: true,
+                    id: this.id,
+                    name: this.name,
+                    status: this.status
+
+                },
+                type: "POST"
+            });
+
+            return post;
+        }
+    };
 }
 
-var TasksList = function () {
-	
-	var tasks = [];
+var TasksList = function() {
 
-	return {		
-		enqueue : function (task) {
-			tasks.push(task);
+    var tasks = [];
 
-			return true;
-		},
-		
-		dequeue : function () {
-			if (tasks.length > 0) {
-				
-				return tasks.shift();
-			}
+    return {
+        enqueue: function(task) {
+            tasks.push(task);
 
-			return false;
-		},
+            return true;
+        },
 
-		toArray : function () {
+        dequeue: function() {
+            if (tasks.length > 0) {
 
-			return tasks;
-		},
+                return tasks.shift();
+            }
 
-		reset : function () {
-			while (tasks.length > 0) {
-				tasks.pop();
-			}
-		}
-	};
+            return false;
+        },
+
+        toArray: function() {
+
+            return tasks;
+        },
+
+        reset: function() {
+            while (tasks.length > 0) {
+                tasks.pop();
+            }
+        }
+    };
 }
- 
+
 
 /**
  * Basic Structures
- */ 
+ */
 var scheduledList = new function() {
-	var list = new TasksList();	 
+    var list = new TasksList();
 
-	return {
-		add : function (argId, argName, argStatus) {
-			var task = new Task(
-	 			argId,
-	 			argName,
-	 			argStatus
- 			);
+    return {
+        add: function(argId, argName, argStatus) {
+            var task = new Task(
+                argId,
+                argName,
+                argStatus
+            );
 
-			list.enqueue(task);	
+            list.enqueue(task);
 
-			$('#schedule-list').append(
-				'<li class="list-group-item" data-id="' + task.id +'">' +
-            		'<span class="badge">' + task.id +'</span>' + task.name +
-         		'</li>'
-     		);
+            $('#schedule-list').append(
+                '<li class="list-group-item" data-id="' + task.id + '">' +
+                '<span class="badge">' + task.id + '</span>' + task.name +
+                '</li>'
+            );
 
-         	return true;
-		},
+            return true;
+        },
 
-		clear : function () {
+        clear: function() {
 
-			$('#schedule-list li').remove();
+            $('#schedule-list li').remove();
 
-			return list.reset();		
-		},
+            return list.reset();
+        },
 
-		next : function () {
-			var task = list.dequeue();
+        next: function() {
+            var task = list.dequeue();
 
-			if (task) {
-				runningList.run(task);			
-			}			
-		},		
+            if (task) {
+                runningList.run(task);
+            }
+        },
 
-		toArray : function () {
+        toArray: function() {
 
-			return list.toArray();
-		}
-	};
+            return list.toArray();
+        }
+    };
 }
 
 var runningList = new function() {
 
-	return {
-		run : function (task) {		
-			$('#running-list').append(
-				$('li[data-id='+ task.id +']')
-     		);
-         	
-			$('#log-txt').val(
-				$('#log-txt').val() + '\r\nStarting task with id = ' + task.id
-			);
+    return {
+        run: function(task) {
+            $('#running-list').append(
+                $('li[data-id=' + task.id + ']')
+            );
 
-			task.status = 'executing';
+            $('#log-txt').val(
+                $('#log-txt').val() + '\r\nStarting task with id = ' + task.id
+            );
 
-			task.execute().done(
-				function (response) {
-					$('#log-txt').val(
-						$('#log-txt').val() + '\r\nFinished task with id = ' + task.id
-					);
+            task.status = 'executing';
 
-					$('#log-txt').val(
-						$('#log-txt').val() + '\r\n' +
-						'success : ' + response.success + ', responseTime : ' + response.responseTime
-					);
+            task.execute().done(
+                function(response) {
+                    $('#log-txt').val(
+                        $('#log-txt').val() + '\r\nFinished task with id = ' + task.id
+                    );
 
-					$('#log-txt').val(
-						$('#log-txt').val() + '\r\n'
-					);
+                    $('#log-txt').val(
+                        $('#log-txt').val() + '\r\n' +
+                        'success : ' + response.success + ', responseTime : ' + response.responseTime
+                    );
 
-					task.status = 'finished';
+                    $('#log-txt').val(
+                        $('#log-txt').val() + '\r\n'
+                    );
 
-					finishedList.add(task);
+                    task.status = 'finished';
 
-					scheduledList.next();
-				}
-			);							
-		},
+                    finishedList.add(task);
 
-		clear : function () {
-			$('#running-list li').remove();
-		},
-	};
+                    scheduledList.next();
+                }
+            );
+        },
+
+        clear: function() {
+            $('#running-list li').remove();
+        },
+    };
 }
 
 var finishedList = new function() {
 
-	return {
-		add : function (task) {		
-			$('#finished-list').append(
-				$('li[data-id='+ task.id +']')
-     		);						
-		},
+    return {
+        add: function(task) {
+            $('#finished-list').append(
+                $('li[data-id=' + task.id + ']')
+            );
+        },
 
-		clear : function () {
-			$('#finished-list li').remove();
-		},
-	};
+        clear: function() {
+            $('#finished-list li').remove();
+        },
+    };
 }
 
 /**
  * General functions
  */
 function generate() {
-	 reset();
+    reset();
 
-	 var minimumTasks = Number.parseInt($('#tasks-number-inp').data('min'));
-	 var tasksNumber = Number.parseInt($('#tasks-number-inp').val());
+    var minimumTasks = Number.parseInt($('#tasks-number-inp').data('min'));
+    var tasksNumber = Number.parseInt($('#tasks-number-inp').val());
 
-	 for (var i = 1, taskIndex = 1; i <= tasksNumber; ++i, ++taskIndex) {
-	 	scheduledList.add(	 		 
- 			taskIndex,
- 			'Task',
- 			'scheduled' 			 
- 		);
-	 }
+    for (var i = 1, taskIndex = 1; i <= tasksNumber; ++i, ++taskIndex) {
+        scheduledList.add(
+            taskIndex,
+            'Task',
+            'scheduled'
+        );
+    }
 }
 
 function run() {
-	scheduledList.next();
+    scheduledList.next();
 }
 
 function reset() {
-	scheduledList.clear();
-	runningList.clear();
-	finishedList.clear();
+    scheduledList.clear();
+    runningList.clear();
+    finishedList.clear();
 }
 
 /**
  * Register events
  */
 $(
-	function () {
-		 
-		$('#generate-btn').on(
-			'click',
-			function (event) {
-				generate();
-				$('#run-btn').prop('disabled', false);
-				$('#generate-btn').prop('disabled', true);
-				$('#reset-btn').prop('disabled', false);
-			}
-		);
+    function() {
 
-		$('#run-btn').on(
-			'click',
-			function (e) {
-				$('#run-btn').prop('disabled', true);
-				$('#generate-btn').prop('disabled', true);
-				$('#reset-btn').prop('disabled', true);
+        $('#generate-btn').on(
+            'click',
+            function(event) {
+                generate();
+                $('#run-btn').prop('disabled', false);
+                $('#generate-btn').prop('disabled', true);
+                $('#reset-btn').prop('disabled', false);
+            }
+        );
 
-				run();	
+        $('#run-btn').on(
+            'click',
+            function(e) {
+                $('#run-btn').prop('disabled', true);
+                $('#generate-btn').prop('disabled', true);
+                $('#reset-btn').prop('disabled', true);
 
-				$('#reset-btn').prop('disabled', false);			
-			}
-		);
+                run();
 
-		$('#reset-btn').on(
-			'click',
-			function (e) {
-				reset();
-				$('#generate-btn').prop('disabled', false);
-				$('#run-btn').prop('disabled', true);
-				$('#reset-btn').prop('disabled', true);
-				$('#log-txt').val('');
-			}
-		);
+                $('#reset-btn').prop('disabled', false);
+            }
+        );
 
-	}
+        $('#reset-btn').on(
+            'click',
+            function(e) {
+                reset();
+                $('#generate-btn').prop('disabled', false);
+                $('#run-btn').prop('disabled', true);
+                $('#reset-btn').prop('disabled', true);
+                $('#log-txt').val('');
+            }
+        );
+
+    }
 );
-
